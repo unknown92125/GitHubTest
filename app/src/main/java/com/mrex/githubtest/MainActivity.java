45fbcc2d79e8c1523cd8d0dc5e9ba521a109ef1c
unknown92125 <57166697+unknown92125@users.noreply.github.com>
2019-10-30 15:56:44 +0900

    Update MainActivity.java

--- app/src/main/java/com/mrex/githubtest/MainActivity.java
+++ app/src/main/java/com/mrex/githubtest/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv=findViewById(R.id.tv);
-        tv=setText("Nice");
+        tv.setText("Nice");
     }
 }
